82a21e372a3fb03dac80a31f0c8fb13eccea0e99
Jennifer Zheng <zjy258085@gmail.com>
2025-04-15 11:53:24 -0400

    add hero

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        //create a new world with 600x400 cells with a cell size of 1x1 pixels
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
     }
 }
